cdbc11fd58f939c4ce796a233a132248c9258505
Tri0nic <androyd0007@yandex.ru>
2023-04-30 21:42:48 +0300

    create ASP.NET project

---
+++ draw-route-asp.net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace draw_route_asp.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
