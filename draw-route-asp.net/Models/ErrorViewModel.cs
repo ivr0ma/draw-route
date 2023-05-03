e9e00abf28f3b554576735c1d3083d8c1d8cbc9b
ivr0ma <ivroma.ru@yandex.ru>
2023-05-04 01:06:49 +0300

    add front-end

--- draw-route-asp.net/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace draw_route_asp.net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
